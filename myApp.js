f68b9db46326236969abb90ecc1ab390e5addb5b
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-21 03:28:33 -0300

    incompatibilities fixed

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
-
+let mongoose = require("mongoose");
 let Person;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
